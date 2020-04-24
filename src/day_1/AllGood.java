9e7a9fdc399d2898a6022242c6456b1733d3f13f
Abdullah Ustun <63824707+korkutustun@users.noreply.github.com>
2020-04-24 16:29:52 -0400

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -4,5 +4,9 @@ public class AllGood {
     public static void main(String[] args) {
         // I hope everything is GOOD
         //This line added from github
+        
+        
+        
+        
     }
 }
