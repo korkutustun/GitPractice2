b376c99632af1f8fb369d106b9a94cf0f58888a3
Abdullah Ustun <63824707+korkutustun@users.noreply.github.com>
2020-04-28 15:52:12 -0400

    Update HappayBirday.java

--- src/day_1/HappayBirday.java
+++ src/day_1/HappayBirday.java
@@ -3,5 +3,11 @@ package day_1;
 public class HappayBirday {
     public static void main(String[] args) {
         System.out.println("happy birtday");
+        
+        System.out.println("dogum gunun kutlu olsun");
+        
+        System.out.println("dogum gunun kutlu olsun add from githubb");
+        
+        
     }
 }
